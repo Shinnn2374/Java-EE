e41c2cf3ca5da208e2e56e03a0193b1cf2d02bcf
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-07-19 16:27:00 +0500

    ...

--- firstMicroService/src/main/java/sia/firstMicroService/controller/MainController.java
+++ firstMicroService/src/main/java/sia/firstMicroService/controller/MainController.java
@@ -1,20 +1,18 @@
 package sia.firstMicroService.controller;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sia.firstMicroService.service.GoogleClient;
 
 @RestController
 @AllArgsConstructor
-@RequiredArgsConstructor
 public class MainController {
 
-    private GoogleClient googleClient;
+    private  final GoogleClient googleClient;
 
     @GetMapping(path = "/")
-    private String getMainPage(){
+    public String getMainPage(){
         return  googleClient.getMainPage();
     }
 }
