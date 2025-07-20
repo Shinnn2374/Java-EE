234cc7e0362da531397e7149ee90176215a08386
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-07-21 00:40:59 +0500

    ...

--- ServiceDescovery/src/main/java/sia/ServiceDiscovery/ServiceDiscoveryApplication.java
+++ ServiceDescovery/src/main/java/sia/ServiceDiscovery/ServiceDiscoveryApplication.java
@@ -2,7 +2,9 @@ package sia.ServiceDiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
 
